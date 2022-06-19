66e26238544fd53c4801b0526b9c5ee1295cdad1
Borsuk Dmytro <dmytro.borsuk03@gmail.com>
2022-06-19 15:59:25 +0300

    add buttons

---
+++ script.js
@@ -0,0 +1,9 @@
+const buttonStart = document.querySelector('.button-development-start')
+buttonStart.addEventListener('click', (e) => {
+    console.log('Clicked', e.target)
+})
+
+const buttonStop = document.querySelector('.button-development-stop')
+buttonStop.addEventListener('click', (e) => {
+    console.log('clicked', e)
+})
