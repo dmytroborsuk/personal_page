603ca249981455b43f638373f288dd9b88800c9e
Borsuk Dmytro <dmytro.borsuk03@gmail.com>
2022-06-25 16:17:01 +0300

    add script code-block

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const buttonStart = document.querySelector('.button-development-start')
+const codeBlock = document.querySelector('.code-block')
+
 buttonStart.addEventListener('click', (e) => {
     console.log('Clicked', e.target)
+    codeBlock.classList.remove('hide')
+    codeBlock.classList.add('show')
 })
 
 const buttonStop = document.querySelector('.button-development-stop')
 buttonStop.addEventListener('click', (e) => {
     console.log('clicked', e)
+    codeBlock.classList.remove('show')
+    codeBlock.classList.add('hide')
+})
+
+document.addEventListener('DOMContentLoaded', function (e) {
+    console.log('Loaded', e)
 })
